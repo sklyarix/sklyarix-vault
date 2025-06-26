74a6ddd9e6619d70ebee34d3a0bdd4465a02f31f
Sklyarenko Aleksandr <work.sklyar@yandex.ru>
2025-06-27 00:23:24 +0300

    fix: use VITE_API_URL instead of process.env

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -2,7 +2,8 @@ import TransactionList from "../../components/transaction/TransactionList.tsx";
 import TransactionSumMonth from "../../components/transaction/TransactionSumMonth.tsx";
 
 const Home = () => {
-  console.log("API URL:", process.env.API_URL);
+  console.log("API URL:", import.meta.env.VITE_API_URL;);
+
   return (
     <>
       <div className="mb-4">
